92fa1ef7d3f7c697a57eeaa3030166bb035edcff
Your Name <you@example.com>
2021-03-02 17:43:15 +0100

    fix

--- extern/clone_dependencies.sh
+++ extern/clone_dependencies.sh
@@ -1,7 +1,6 @@
 git clone git@github.com:fnc12/sqlite_orm.git
 git clone git@github.com:midjji/JKQtPlotter.git
-git clone git@github.com:midjji/ceres-solver.git
+git clone git@github.com:midjji/ceres-solver.git ceres/ceres-solver
 git clone git@github.com:midjji/convenient_multithreaded_qt_gui.git
 git clone https://github.com/onqtam/doctest.git
-
 git clone git@github.com:midjji/cmake.git
