60c26cad58fd389dbffbd6b03a335cd97845cbe6
Your Name <you@example.com>
2021-03-03 11:03:13 +0100

    finally fixed the dependencies

--- extern/clone_dependencies.sh
+++ extern/clone_dependencies.sh
@@ -1,6 +1,16 @@
-git clone git@github.com:fnc12/sqlite_orm.git
-git clone git@github.com:midjji/JKQtPlotter.git
-git clone git@github.com:midjji/ceres-solver.git ceres/ceres-solver
-git clone git@github.com:midjji/convenient_multithreaded_qt_gui.git
+
+
 git clone https://github.com/onqtam/doctest.git
+git clone https://github.com/fnc12/sqlite_orm.git
+git clone https://github.com/ceres-solver/ceres-solver.git ceres/ceres-solver
+
+
+if [ "$(whoami)" == "mikael" ]; then
+git clone git@github.com:midjji/JKQtPlotter.git
+git clone git@github.com:midjji/convenient_multithreaded_qt_gui.git
 git clone git@github.com:midjji/cmake.git
+else
+git clone https://github.com/midjji/JKQtPlotter.git
+git clone https://github.com/midjji/convenient_multithreaded_qt_gui.git
+git clone https://github.com/midjji/cmake.git
+fi
